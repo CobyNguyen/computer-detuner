d5f11c690b90a28cdca422b897705d478081d1e1
noaheclifford <noaheclifford@gmail.com>
2025-11-14 14:37:27 -0600

    Update storage_module.py
    add connect_storage function
    lets you define an intensity
    currently runs for 10 seconds

--- modules/storage_module.py
+++ modules/storage_module.py
@@ -1,13 +1,28 @@
 # inital test for hard drive detuner
+import time
 
-for repeat in range(1000):
+intensityFileAmount = [5, 10, 20, 50, 100]
+demoTime : int = 10
 
-    for i in range(3):
-        with open("glorp_clone_" + str(i + 1) + ".txt", "w") as file:
-            with open("resources/glorp.txt", "r") as file2: #path for glorp not tested
-                value = str(file2.read())
-                file.write(value)
-
-    for i in range(3):
-        with open("glorp_clone_" + str(i + 1) + ".txt", "w") as file:
-            file.write("")
+def connect_storage(val : int = 1):
+    val = max(1, val)
+    val = min(5, val)
+    
+    print("Intensity is", val)
+    
+    intensity = intensityFileAmount[val]
+    
+    for repeat in range(demoTime * 10):
+    
+        for i in range(intensity):
+            with open("glorp_clone_" + str(i + 1) + ".txt", "w") as file:
+                file.write("")
+        
+        for i in range(intensity):
+            with open("glorp_clone_" + str(i + 1) + ".txt", "w") as file:
+                
+                with open("resources/glorp.txt", "r") as file2: #path for glorp not tested
+                    value = str(file2.read())
+                    file.write(value)
+        
+        time.sleep(0.1)
