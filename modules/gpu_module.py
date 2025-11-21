8bdd1446f87bf0f90feab9be3bc4063fd0a1ada1
Mark Oluoni <oluonidolapo@gmail.com>
2025-11-21 13:41:31 -0600

    update file

--- modules/gpu_module.py
+++ modules/gpu_module.py
@@ -3,7 +3,20 @@
 Expose `connect_gpu(intensity:int) -> dict` for the UI to call.
 """
 import time
+import random
 
+INTENSITY_MAP = {
+	1: 50,
+	2: 100,
+	3: 200,
+	4: 500,
+	5: 1000
+}
+
+def connect_gpu(intensity: int = 5):
+	size = INTENSITY_MAP(intensity, 100)
+
+	
 
 def connect_gpu(intensity: int = 5):
 	delay = 0.4 + (intensity / 100.0) * 1.2
