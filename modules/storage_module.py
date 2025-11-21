f89fdf3cdae5a9fda965b38b3abd68379b864948
noaheclifford <noaheclifford@gmail.com>
2025-11-21 13:36:40 -0600

    Update storage_module.py
    make the storage module more intense

--- modules/storage_module.py
+++ modules/storage_module.py
@@ -1,28 +1,28 @@
 # inital test for hard drive detuner
 import time
 
-intensityFileAmount = [5, 10, 20, 50, 100]
+intensityFileAmount = [10, 50, 100, 250, 500]
 demoTime : int = 10
 
-def connect_storage(val : int = 1):
-    val = max(1, val)
-    val = min(5, val)
+def connect_storage(intensity : int = 1):
+    intensity = max(1, val)
+    intensity = min(5, val)
     
     print("Intensity is", val)
     
-    intensity = intensityFileAmount[val]
+    moduleIntensity = intensityFileAmount[val]
     
-    for repeat in range(demoTime * 10):
+    for repeat in range(demoTime * 1000):
     
-        for i in range(intensity):
+        for i in range(moduleIntensity):
             with open("glorp_clone_" + str(i + 1) + ".txt", "w") as file:
                 file.write("")
         
-        for i in range(intensity):
+        for i in range(moduleIntensity):
             with open("glorp_clone_" + str(i + 1) + ".txt", "w") as file:
                 
-                with open("resources/glorp.txt", "r") as file2: #path for glorp not tested
+                with open("modules/resources/glorp.txt", "r") as file2: #path for glorp not tested
                     value = str(file2.read())
                     file.write(value)
         
-        time.sleep(0.1)
+        time.sleep(0.001)
